50e8738c0c7a72603a938400a2d0a4b778bd2f62
Jorge Díez Tubet <j.diez@adacsl.com>
2024-08-14 15:46:28 +0200

    Creada Azure Function API.

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,8 @@
-windows.addEventListener('DOMContentLoaded', (event) =>{
+window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
